9fcb174ad48adf0e0284ebf0a8356a93031cce6d
loh <faraon.fizik@bk.ru>
2023-08-24 01:47:28 +0500

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+var x = +prompt('age')
+
+
+
+
+if (x < 18 ) {
+    alert('melkiy eshe');
+}else if (x < 50) {
+    alert('work');
+}else if (x < 59) {
+    alert('soon penisai');
+}else if (x < 100) {
+    alert('stariy');
+}else if (x > 100 ) {
+    alert('error');
+}if( isNaN(x)){
+    alert('it is not age')
+}
+
+
